be17b74f7126cc4a1893e41b3c3cb084d7ca84f5
artelse <787801+artelse@users.noreply.github.com>
2022-10-10 17:19:07 +0200

    Rudimentary ISBN support
    Early stages, lots of debugging info. Mainly code for ISBN validity check. (First elisp code ever ;)

--- pdf-drop-mode.el
+++ pdf-drop-mode.el
@@ -36,7 +36,7 @@
 ;;                the first page (PDF is transformed into text using the pdftotext
 ;;                utility)
 ;; - doi/title: ask the user to enter the title of the file and query the crossref
-;;              database to try to get a correspondong DOI. 
+;;              database to try to get a correspondong DOI.
 ;; - doi/user: ask user to enter the DOI of the PDF.
 ;; - arxiv/content: search the content of the file to try to locate a arxiv-id regex
 ;;                  on the first page (PDF is transformed into text using the
@@ -48,7 +48,7 @@
 
 
 ;;; News
-;; ae_test
+
 ;; Version 0.2.0
 ;; - API modification for taling into account arxiv papers.
 ;; - arxiv papers are now identified.
@@ -56,20 +56,22 @@
 ;; Version 0.1.0
 ;; Initial release
 
+;;; Commentary:
+;; Add ISBN support.
+
 ;;; Code:
 (require 'url-http)
 
 (defcustom pdf-drop-pdftotext-path "pdftotext"
-  "Path to the pdftotext executable (used to extract DOI from the
-content of a pdf)."
+  "Path to the pdftotext executable (used to extract DOI from the content of a pdf)."
   :type 'file)
 
 (defcustom pdf-drop-exiftool-path "exiftool"
-  "Path to the exiftool executable (used to extract TITLE from
-pdf metadata)."
+  "Path to the exiftool executable (used to extract TITLE from pdf metadata)."
   :type 'file)
 
-(defcustom pdf-drop-search-methods '(doi/metadata
+(defcustom pdf-drop-search-methods '(isbn/content
+                                     doi/metadata
                                      doi/content
                                      arxiv/content
                                      doi/title
@@ -83,16 +85,23 @@ Called function is provided with the filename and the associated
 doi (or nil) as arguments."
   :type 'function)
 
+(defvar pdf-drop--isbn-regex
+  "\\(?1:ISBN[-]*\\(1[03]\\)*[ ]*\\(: \\)*\\)\\(?2:\\([0-9Xx][- ]*\\)\\{13\\}\\|\\([0-9Xx][- ]*\\)\\{10\\}\\)"
+  "Regular expression matching an ISBN."
+  )
+;; "\\(ISBN[-]*\\(1[03]\\)*[ ]*\\(: \\)\\{0,1\\}\\)*\\(\\([0-9Xx][- ]*\\)\\{13\\}\\|\\([0-9Xx][- ]*\\)\\{10\\}\\)"
+;; "\\(?1:ISBN-*\\(1[03]\\)*\\(: \\)\\{0,1\\}\\)\\(?2:\\([0-9Xx][ -]*\\)\\{13\\}\\|\\([0-9Xx][ -]*\\)\\{10\\}\\)"
+
 ;; This will fail if the DOI is split over two lines. Such case may happend when
 ;; pdftotext is used to convert the PDF to text.
 (defvar pdf-drop--doi-regex "\\(10\\.[0-9]\\{4,9\\}/[-+._;()/:A-Z0-9]+\\)"
   "Regular expressions matching a DOI.")
 
 (defvar pdf-drop--crossref-search-url "http://search.crossref.org/dois?q=%s&rows=10"
-  "URL to the crossref search API")
+  "URL to the crossref search API.")
 
 (defvar pdf-drop--crossref-query-url "http://dx.doi.org/%s"
-  "URL to the crossref query API")
+  "URL to the crossref query API.")
 
 ;; Adapted from https://github.com/jkitchin/org-ref (org-ref-arxiv.el)
 ;; Copyright (C) 2015-2021 John Kitchin (GPL v2)
@@ -100,14 +109,62 @@ doi (or nil) as arguments."
     "Regular expressions matching an arxiv-id (new format, 2007).")
 
 (defvar pdf-drop--arxiv-search-url "https://ui.adsabs.harvard.edu/abs/%s/exportcitation"
-  "URL to arxiv searh yAPI")
+  "URL to arxiv searh yAPI.")
 
 (defvar pdf-drop--arxiv-query-url "http://adsabs.harvard.edu/cgi-bin/bib_query?arXiv:%s"
-  "URL to the arxiv query API")
+  "URL to the arxiv query API.")
+
+(defun pdf-drop-validate-isbn (isbn)
+  "Check the validity of ISBN by means of checksum."
+
+  (catch 'test
+    ;; Get checksum and remove non ISBN characters.
+    (let* ((checksum (substring isbn -1))
+           (isbn (substring (replace-regexp-in-string "[- ]" "" isbn) 0 -1))
+           (check 0))
+      ;; (message "isbn: %s" isbn)
+      ;; (message "checksum: %s" checksum)
+      ;; map isbn string to number list
+      (setq isbn (cl-map 'list 'string-to-number (mapcar 'char-to-string isbn)))
+      ;;(message "isbnnumlist: %s" isbn)
+
+      ;; Check ISBN-10 or ISBN-13 format
+      (if (= (length isbn) 9) ; ISBN-10
+          (progn
+            ;;(message "ISBN10") ; calc the checksum
+            (cl-loop for i downfrom 10 to 2
+                     sum (* i (car (nthcdr (- 10 i) isbn))) into sum
+                     finally (setq check sum))
+            ;;(message "sum: %d" check)
+            (setq check (- 11 (% check 11)))
+            (cond ((= check 10)(setq check "X"))
+                  ((= check 11)(setq check "0"))
+                  (t (setq check (number-to-string check)))))
+        ;;(message "check: %s" check))
+        ;;(message "ISBN13")
+        (cl-loop for i from 0 to 11
+                 if (cl-oddp i)
+                 sum (* (+ (* (% i 2) 2) 1) (car (nthcdr i isbn))) into sum
+                 else
+                 sum (* (+ (* (% i 2) 2) 1) (car (nthcdr i isbn))) into sum
+                 finally (setq check sum))
+        ;;(message "check: %d" check)
+        (setq check (- 10 (% check 10)))
+        (if (= check 10)(setq check "0")(setq check (number-to-string check))))
+      ;;(message "checksum: %s" check))
+      (if (equal check checksum)
+          (message "checksum ok"))
+      )))
+;;(pdf-drop-validate-isbn isbn)
+
+(setq isbn "0-520-23249-6")
+(setq isbn "1-56389-016-X")
+(setq isbn "1 56389 016 X")
+(setq isbn "978-3-540-19568-9")
 
 (defun pdf-drop-validate-doi (doi)
-  "Check if DOI is valid by querying crossref"
-  
+  "Check if DOI is valid by querying crossref."
+
   (let* ((url (format "https://hdl.handle.net/%s?noredirect" doi))
          (status (url-http-symbol-value-in-buffer 'url-http-response-status
                                                   (url-retrieve-synchronously url))))
@@ -116,11 +173,11 @@ doi (or nil) as arguments."
 ;; Adapted from https://github.com/jkitchin/org-ref (org-ref-arxiv.el)
 ;; Copyright (C) 2015-2021 John Kitchin (GPL v2)
 (defun pdf-drop-validate-arxiv-id (arxiv-id)
-  "Check if ARXIV-ID is valid by querying arXiv"
+  "Check if ARXIV-ID is valid by querying arXiv."
   
   (let* ((url (format pdf-drop--arxiv-query-url arxiv-id))
          (status (url-http-symbol-value-in-buffer 'url-http-response-status
-                                   (url-retrieve-synchronously url))))
+                                                  (url-retrieve-synchronously url))))
     (eq 200 status)))
 
 ;; Adapted from https://github.com/jkitchin/org-ref (doi-utils.el)
@@ -128,11 +185,11 @@ doi (or nil) as arguments."
 (defun pdf-drop-get-doi-from-title (title &optional score-threshold)
   "Query crossref.org for TITLE and return the corresponding doi.
 
-When multiple canditates are found, the one with the smallest
-levenshtein distance is chosen if its distance to TITLE is less
-than SCORE-THRESHOLD. Else, user is asked to choose among 5 best
-candidates. If no SCORE-THRESHOLD is given, the first candidate
-doi is returned."
+  When multiple canditates are found, the one with the smallest
+  levenshtein distance is chosen if its distance to TITLE is less
+  than SCORE-THRESHOLD.  Else, user is asked to choose among 5 best
+  candidates.  If no SCORE-THRESHOLD is given, the first candidate
+  doi is returned."
 
   (interactive "sTitle: ")
   (with-current-buffer (url-retrieve-synchronously
@@ -143,19 +200,19 @@ doi is returned."
       (goto-char (point-min))
       (while (re-search-forward "<i>\\|</i>" nil t)
       	(replace-match ""))
-	  (goto-char (point-min))
-	  (while (re-search-forward "&amp;" nil t)
-	    (replace-match "&"))
-      (goto-char (point-min))
-      (while (re-search-forward "&quot;" nil t)
-      	(replace-match "\\\"" nil t)))
+	    (goto-char (point-min))
+	    (while (re-search-forward "&amp;" nil t)
+	(replace-match "&"))
+         (goto-char (point-min))
+         (while (re-search-forward "&quot;" nil t)
+           (replace-match "\\\"" nil t)))
     (setq raw-json-string
           (buffer-substring url-http-end-of-headers (point-max)))
     (setq json-string
           (decode-coding-string (encode-coding-string raw-json-string 'utf-8) 'utf-8))
     (setq json-data
           (json-read-from-string json-string))
-    
+
     (let* (;; (score-threshold (or score-threshold 10))
            (candidates (mapcar (lambda (x)
                                  (let* ((x-full (cdr (assoc 'fullCitation x)))
@@ -180,8 +237,22 @@ doi is returned."
                          best-match
                        (completing-read "Matched title: " sorted-candidates nil nil
                                         (if (< best-score 10) best-match nil))))
-	       (doi (cdr (assoc selected sorted-candidates))))
+	         (doi (cdr (assoc selected sorted-candidates))))
       doi)))
+
+(defun pdf-drop-get-isbn-from-content (file)
+  "Extract isbn from a pdf FILE content (after conversion to text)."
+
+  (with-temp-buffer
+    (let ((status (call-process pdf-drop-pdftotext-path nil '(t nil) nil
+                                "-f" "1" "-l" "6" "-enc" "UTF-8" file "-")))
+      (goto-char (point-min))
+      (if (and (eq status 0)
+               (> (buffer-size) 0)
+               ;;(message "buffer-size")
+               (search-forward-regexp pdf-drop--isbn-regex nil t))
+          (string-trim (match-string 2) "[ \\t\\n\\r()]+" "[ \\t\\n\\r()]+")
+        ))))
 
 (defun pdf-drop-get-doi-from-content (file)
   "Extract doi from a pdf FILE content (after conversion to text)."
@@ -195,9 +266,9 @@ doi is returned."
                (search-forward-regexp pdf-drop--doi-regex nil t))
           (string-trim (match-string 1) "[ \\t\\n\\r()]+" "[ \\t\\n\\r()]+")
         ))))
-      
+
 (defun pdf-drop-get-tag-from-metadata (file tag)
-  "Get TAG from file metadata."
+  "Get TAG from FILE metadata."
 
   (with-temp-buffer
     (let ((status (call-process pdf-drop-exiftool-path nil '(t nil) nil
@@ -207,12 +278,12 @@ doi is returned."
   
 
 (defun pdf-drop-get-doi-from-metadata (file)
-  "Get DOI from file metadata."
+  "Get DOI from FILE metadata."
   
   (pdf-drop-get-tag-from-metadata file "DOI"))
 
 (defun pdf-drop-get-title-from-metadata (file)
-  "Get TITLE from file metadata."
+  "Get TITLE from FILE metadata."
   
   (pdf-drop-get-tag-from-metadata file "TITLE"))
 
@@ -243,13 +314,22 @@ doi is returned."
     (pdf-drop--process file)))
 
 (defun pdf-drop--process (file)
-  "Try to get the DOI associated to file."
+  "Try to get the DOI associated to FILE."
 
   (let ((file-id))
     (catch 'found
       (dolist (method pdf-drop-search-methods)
+        (message "method: %s" method)
+        (cond ((eq method 'isbn/content)
+               (let ((isbn (pdf-drop-get-isbn-from-content file)))
+                 (message "ISBN: %s" isbn)
+                 (if (and isbn (pdf-drop-validate-isbn isbn))
+                     (progn
+                       (setq file-id `(isbn . ,isbn))
+                       (throw 'found file-id))
+                   (message "isbn/content method failed for %s" file))))
 
-        (cond ((eq method 'doi/content)
+              ((eq method 'doi/content)
                (let ((doi (pdf-drop-get-doi-from-content file)))
                  (if (and doi (pdf-drop-validate-doi doi))
                      (progn
@@ -257,7 +337,7 @@ doi is returned."
                        (throw 'found file-id))
                    (message "doi/content method failed for %s" file))))
 
-              ((eq method 'arxiv/content) 
+              ((eq method 'arxiv/content)
                (let ((arxiv-id (pdf-drop-get-arxiv-id-from-content file)))
                  (if (and arxiv-id (pdf-drop-validate-arxiv-id arxiv-id))
                      (progn
@@ -272,7 +352,7 @@ doi is returned."
                        (setq file-id `(doi . ,doi))
                        (throw 'found file-id))
                    (message "doi/metadata method failed for %s" file))))
-            
+
               ((eq method 'doi/title)
                (let ((buffer (find-file file)))
                  (with-current-buffer buffer
@@ -302,7 +382,7 @@ doi is returned."
                            (message "doi/user method failed for %s" file)))
                      (kill-buffer buffer))))))))
 
-    (if file-id 
+    (if file-id
         (progn
           (message "ID found: %s" file-id)
           (if pdf-drop-search-hook
@@ -315,7 +395,7 @@ doi is returned."
   
   (let ((url-mime-accept-string "text/bibliography;style=bibtex"))
     (with-current-buffer
-      (url-retrieve-synchronously (format pdf-drop--crossref-query-url doi))
+        (url-retrieve-synchronously (format pdf-drop--crossref-query-url doi))
       (decode-coding-string
        (buffer-substring (string-match "@" (buffer-string)) (point)) 'utf-8))))
 
@@ -332,8 +412,8 @@ doi is returned."
                    (with-current-buffer
                        (url-retrieve-synchronously (format pdf-drop--arxiv-search-url arxiv-code))
                      (when (re-search-forward
-	                        "<textarea.*>\\(.*\\(?:\n.*\\)*?\\(?:\n\\s-*\n\\|\\'\\)\\)</textarea>"
-	                        nil t)
+	                          "<textarea.*>\\(.*\\(?:\n.*\\)*?\\(?:\n\\s-*\n\\|\\'\\)\\)</textarea>"
+	                          nil t)
                        (xml-substitute-special (match-string 1)))))))
     bibtex))
 
@@ -352,4 +432,3 @@ doi is returned."
 
 (provide 'pdf-drop-mode)
 ;;; pdf-drop-mode.el ends here
-
